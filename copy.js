6255147dd16f4d321862ecd0a7b52f2710e6aec9
Joao123433 <joaosantos123433@gmail.com>
2023-11-16 22:03:23 -0300

    Alterando copy.js

--- copy.js
+++ copy.js
@@ -1,7 +1,8 @@
-import { inputResultado } from "./calculate.js"
+import { inputResultado, inputValue } from "./calculate.js"
 
 export function copyPast(ev) {
     ev.preventDefault()
     navigator.clipboard.writeText(inputResultado.value)
     alert("Valor passado para area de tranferencia")
+    inputValue.value = ""
 }
